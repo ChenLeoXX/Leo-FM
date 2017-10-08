74ed04b15d90cb8a2da57abca1b96977dc77eb10
Leo <479711970@qq.com>
2017-10-09 00:20:37 +0800

    修复无法正常进入静态问题，添加部分特效

--- fm.js
+++ fm.js
@@ -1,25 +1,30 @@
 // Ajax 请求
 var audio = new Audio()
 audio.autoplay = true
+audio.volume = 0.6
 var channel
 var sid
-getRanMusic(channel,sid)
 //jQ写法
+getRanMusic(channel, sid)
 function getRanMusic() {
   $.get('https://jirenguapi.applinzi.com/fm/getSong.php?channel=4', { channel: channel })
     .done(function (song) {
       musicObj = JSON.parse(song).song[0]
       loadMusic(musicObj)
       getNum()
-    }).fail(function(){
-            getRanMusic()
+    }).fail(function () {
+      getRanMusic()
     })
-    $.get('https://jirenguapi.applinzi.com/fm/getLyric.php',{sid:sid})
-    .done(function(rel){
-      var lyric = JSON.parse(rel).lyric
-      console.log(lyric)
-    })   
 }
+// 歌词获取
+function getLrc() {
+  $.get('https://jirenguapi.applinzi.com/fm/getLyric.php', { sid: sid })
+    .done(function (musicObj) {
+      var lyric = JSON.parse(musicObj).lyric
+      console.log(lyric)
+    })
+}
+
 //原生常规写法
 // var xhr = new XMLHttpRequest()
 // xhr.open('GET', 'http://api.jirengu.com/fm/getSong.php?',{ channel: channel},true)
@@ -36,12 +41,16 @@ function getRanMusic() {
 //   console.log(musicObj)
 //   loadMusic(musicObj)
 // }
-function loadMusic() {
-  sid = musicObj.sid
+function loadMusic(musicObj) {
   audio.src = musicObj.url
+  sid = musicObj.sid
+  getLrc(sid)
   $('.songauthor').text(musicObj.artist)
   $('.author p').text(musicObj.title)
   $('.album-img img').attr('src', musicObj.picture)
+  //静止状态下的，歌曲信息
+  $('.static').children('p').eq(0).text(musicObj.title)
+  $('.static').children('p').eq(1).text(musicObj.artist)
 }
 //点击专辑切换歌曲功能
 $('.albums li').on('click', function (e) {
@@ -74,17 +83,21 @@ $('.like').on('click', function () {
 })
 //收听量，收藏数。
 function getNum() {
-  $('.music-info span').eq(0).text(' '+(Math.floor(Math.random()*4000)+1000))
-  $('.music-info span').eq(1).text(' '+(Math.floor(Math.random()*999 )+100))
+  $('.music-info span').eq(0).text(' ' + (Math.floor(Math.random() * 4000) + 1000))
+  $('.music-info span').eq(1).text(' ' + (Math.floor(Math.random() * 999) + 100))
 }
 //进度条
 audio.onplaying = function () {
   process = setInterval(function () {
     $('.current').css('width', audio.currentTime / audio.duration * 100 + '%')
+    //静止状态进度条
+    $('.static-process').css('width', audio.currentTime / audio.duration * 100 + '%')
   }, 1000)
+
   $('.pause i').removeClass('icon-play')
   $('.pause i').addClass('icon-pause')
 }
+
 //点击快进
 $('.total').on('click', function (e) {
   var rate = e.offsetX / parseInt(getComputedStyle(this).width)
@@ -95,6 +108,11 @@ audio.ontimeupdate = function () {
   var min = Math.floor(audio.currentTime / 60)
   var sec = Math.floor(audio.currentTime) % 60 + ''
   $('.time').text(function () {
+    sec = sec.length === 2 ? sec : '0' + sec
+    return min + ':' + sec
+  })
+  //静止状态下的时间
+  $('.static-time').text(function () {
     sec = sec.length === 2 ? sec : '0' + sec
     return min + ':' + sec
   })
@@ -113,6 +131,16 @@ $('.length').on('click', function (e) {
   $('.voice').css('width', percent + '%')
   audio.volume = e.offsetX / 200
   $('.circle').css('left', e.offsetX + 'px')
+})
+
+// 滑动图标特效
+$('.albums').mouseenter(function () {
+  $('.left').removeClass('icon-trans-left')
+  $('.right').removeClass('icon-trans-right')
+})
+$('.albums').mouseleave(function () {
+  $('.left').addClass('icon-trans-left')
+  $('.right').addClass('icon-trans-right')
 })
 
 // 专辑滑动(使用函数节流解决快速点击导致滑动不准确问题)
@@ -147,36 +175,26 @@ function trans(direction) {
     $('.carrousel').css('transform', 'translateX(' + translateX + 'px)')
   }, 300);
 }
-//全屏监听，进入静止状态
+//全屏监听，进入静止状态,使用mousemove事件，而不是mouseenter。
 var staticTimer
-
-$(window).on('mouseover',function(){
-  if(staticTimer) {
+$(window).mousemove(function () {
+  if (staticTimer) {
     clearTimeout(staticTimer)
   }
-   staticTimer = setTimeout(function (){
+  $('.music-panel').removeClass('active')
+  $('.control').removeClass('active')
+  $('.albums').removeClass('active')
+  $('.album-img img').removeClass('location')
+  $('.static').addClass('active')
+
+  staticTimer = setTimeout(function () {
     $('.music-panel').addClass('active')
     $('.control').addClass('active')
     $('.albums').addClass('active')
     $('.album-img img').addClass('location')
-    $('.static').removeClass('active')     
-},10000)
-    $('.music-panel').removeClass('active')
-    $('.control').removeClass('active')
-    $('.albums').removeClass('active')
-    $('.album-img img').removeClass('location')
-    $('.static').addClass('active')     
-    console.log(this)
+    $('.static').removeClass('active')
+  }, 10000)
 })
-
-// 歌词获取
-function getLrc() {
-  $.get('https://jirenguapi.applinzi.com/fm/getLyric.php',{sid:sid})
-  .done(function(rel){
-    var lyric = JSON.parse(rel).lyric
-    console.log(lyric)
-  })
-}
 
 
 
